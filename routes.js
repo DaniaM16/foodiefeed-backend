337f11f5e21d754155bf416ae7253c5217eaf9f5
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-04 17:08:39 +0200

    GET ROute zum Auslesen von Reviews erstellt

--- routes.js
+++ routes.js
@@ -1,5 +1,17 @@
+const client = require('./db');
 const express = require('express');
 const router = express.Router();
+router.get('/reviews', async(req, res) => {
+    const query = `SELECT * FROM reviews`;
+
+    try {
+    const result = await client.query(query);
+    console.log(result);
+    res.send(result.rows);
+    } catch (err) {
+     console.log(err.stack);
+     }
+})
 
 // eine GET-Anfrage
 router.get('/', async(req, res) => {
