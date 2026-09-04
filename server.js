c995178161fd8009bbe097a37e20568fcd3b70cc
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-04 15:33:36 +0200

    PostgreSQL Datenbankverbindung eingerichtet

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./db');
 const routes = require('./routes');
 
 const app = express();
