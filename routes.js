373f445b54f1bcb45930aff246f2c899fe0b6943
Mina Ranjbaryan <mina@MacBook-Air-von-Mina.local>
2026-09-04 17:16:27 +0200

    Haben Reviews nach Benutzer gesucht, auch erfolgreich

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const client = require('./db');
 const express = require('express');
 const router = express.Router();
+
 router.get('/reviews', async(req, res) => {
     const query = `SELECT * FROM reviews`;
+
 
     try {
     const result = await client.query(query);
@@ -11,7 +13,30 @@ router.get('/reviews', async(req, res) => {
     } catch (err) {
      console.log(err.stack);
      }
-})
+});
+
+
+
+router.get('/users/:id/reviews', async(req, res) => {
+
+    const userId = req.params.id;
+
+    const query = `
+SELECT reviews.*
+FROM reviews
+WHERE user_id = $1
+`;
+
+
+try {
+const result = await client.query(query,[userId]);
+res.send(result.rows);
+} catch (err) {
+ console.log(err.stack);
+ }
+});
+
+
 
 // eine GET-Anfrage
 router.get('/', async(req, res) => {
