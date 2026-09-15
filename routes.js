0f05bdade30b7f67621465c8630f8d78a343f112
Dania Moshtaha <daniamoshtaha@MacBook-Air-von-Dania.local>
2026-09-15 18:27:07 +0200

    GET route für einzelnes Review erstellt

--- routes.js
+++ routes.js
@@ -36,6 +36,23 @@ res.send(result.rows);
  }
 });
 
+// get review mit id
+router.get('/reviews/:id', async(req,res) => {
+    const query = `SELECT * FROM reviews WHERE id = $1`;
+
+    try {
+        const id = req.params.id;
+        const result = await client.query(query, [id])
+        console.log(result)
+        
+        if (result.rowCount == 1)
+            res.send(result.rows[0]);
+        else
+            res.send({ message: "No review found with id=" + id });
+        } catch (err) {
+            console.log("error", err.stack)
+        }
+});
 
 
 // eine GET-Anfrage
